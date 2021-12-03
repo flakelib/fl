bbb567edefc0891c4dd1b7d540af4eeecd140c53
arcnmx <arcnmx@users.noreply.github.com>
2021-12-02 17:28:16 -0800

    ci

--- ci.nix
+++ ci.nix
@@ -1,11 +1,14 @@
 { pkgs, lib, ... }: with lib; let
   flake-check = name: path: pkgs.ci.command {
     name = "${name}-check";
+    resolverPath = toString ./resolver;
+    libPath = toString ./lib;
+    pkgs = toString pkgs.path;
     command = ''
       if [[ $CI_PLATFORM = gh-actions ]]; then
         nix registry add flakes-std github:arcnmx/nix-std
-        nix registry add flakes-resolver path:${toString ./resolver}
-        nix registry add flakes-lib path:${toString ./lib}
+        nix registry add flakes-resolver path:$resolverPath
+        nix registry add flakes-lib path:$libPath
         nix registry add nixpkgs path:$pkgs
       fi
       nix flake check ./${path}
@@ -20,7 +23,7 @@
   resolver-check = flake-check "resolver" "resolver";
 in {
   name = "flakes.nix";
-  ci.version = "nix2.4";
+  ci.version = "nix2.4-broken";
   ci.gh-actions.enable = true;
   cache.cachix.arc.enable = true;
   channels.nixpkgs = "21.11";
