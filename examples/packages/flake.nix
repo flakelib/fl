ae7fe4e5bd8d7210e1c96fb0cc3b8a334280c545
arcnmx <git@git.arcn.mx>
2022-05-21 12:03:36 -0700

    update example for std

--- examples/packages/flake.nix
+++ examples/packages/flake.nix
@@ -18,19 +18,19 @@
       # callPackage resolves arguments from the flake's inputs
       # `pkgs` and `lib` are special arguments that merge all inputs into a combined scope
       hello-wrapper = { writeShellScriptBin, hello, lib }: writeShellScriptBin "hello-wrapper" ''
-        exec ${lib.drv.mainProgram hello} -g example "$@"
+        exec ${lib.Drv.mainProgram hello} -g example "$@"
       '';
       # the hello-wrapper argument is resolved from the definition above
-      hello-reference = { writeShellScriptBin, hello-wrapper, lib'drv'mainProgram }: writeShellScriptBin "hello-reference" ''
-        exec ${lib'drv'mainProgram hello-wrapper} "$@"
+      hello-reference = { writeShellScriptBin, hello-wrapper, lib'Drv'mainProgram }: writeShellScriptBin "hello-reference" ''
+        exec ${lib'Drv'mainProgram hello-wrapper} "$@"
       '';
     };
     # you can also inhibit or customize the default behaviour by making the output a function
     # (the default behaviour is equivalent to `packages = { callPackageSet }: callPackageSet packages { }`)
-    legacyPackages = { callPackages, lib }: lib.function.flip callPackages { } {
+    legacyPackages = { callPackages, lib }: lib.Fn.flip callPackages { } {
       # inputs can be explicitly specified in case of naming conflicts
-      hello-qualified = { nixpkgs'writeShellScriptBin, nixpkgs'hello, std'lib'drv'mainProgram }: nixpkgs'writeShellScriptBin "hello-wrapper" ''
-        exec ${std'lib'drv'mainProgram nixpkgs'hello} -g qualified "$@"
+      hello-qualified = { nixpkgs'writeShellScriptBin, nixpkgs'hello, std'lib'Drv'mainProgram }: nixpkgs'writeShellScriptBin "hello-wrapper" ''
+        exec ${std'lib'Drv'mainProgram nixpkgs'hello} -g qualified "$@"
       '';
     };
   } // {
